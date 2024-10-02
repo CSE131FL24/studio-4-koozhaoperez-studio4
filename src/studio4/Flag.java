2cbfb51901e0277db8f15a45a116f2108eadfbec
davidperez <davidperez@localhost>
2024-10-02 15:27:47 -0500

    Flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,19 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		double[] xOne = {0.25 ,0.35, 0.45};
+		double[] yOne = {0.4 ,0.6, 0.4};
+		double[] xTwo = {0.4 ,0.5, 0.6};
+		double[] yTwo = {0.4 ,0.6, 0.4};
+		double[] xThree = {0.55 ,0.65, 0.75};
+		double[] yThree = {0.4 ,0.6, 0.4};
+	StdDraw.setPenColor(198, 214, 250); //flag
+	StdDraw.filledRectangle(0.5, 0.5, 0.4, 0.25);
+	StdDraw.setPenColor(200, 182, 255); //left
+	StdDraw.filledPolygon(xOne, yOne);
+	StdDraw.setPenColor(231, 198, 255); //middle
+	StdDraw.filledPolygon(xTwo, yTwo);
+	StdDraw.setPenColor(255, 214, 255); //right
+	StdDraw.filledPolygon(xThree, yThree);
 	}
 }
